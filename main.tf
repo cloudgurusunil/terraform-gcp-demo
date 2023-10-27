8c4344f73aa06c5eec21e05a3992eb281a3a99c6
Sunil M <cloudgurusunil@gmail.com>
2023-10-27 21:32:09 +0530

    First Commit

---
+++ main.tf
@@ -0,0 +1,41 @@
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = "default"
+    access_config {
+    }
+  }
+}
+
+resource "google_storage_bucket" "auto-expire" {
+  name          = "auto-expiring-bucket"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 3
+    }
+    action {
+      type = "Delete"
+    }
+  }
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}   
---
+++ providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "5.3.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "lithe-center-400712"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
